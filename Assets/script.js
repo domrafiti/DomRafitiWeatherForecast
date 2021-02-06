130a0502115f77ffe1f1820e84d33789355f6e02
Dom Rafiti <dom.rafiti@gmail.com>
2021-02-06 14:17:56 -0700

    adding final version of files for submission

--- Assets/script.js
+++ Assets/script.js
@@ -1,44 +1,3 @@
-/*
-GIVEN a weather dashboard with form inputs
-WHEN I search for a city
-THEN I am presented with current and future conditions for that city and that city is added to the search history
-WHEN I view current weather conditions for that city
-THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
-WHEN I view the UV index
-THEN I am presented with a color that indicates whether the conditions are favorable, moderate, or severe
-WHEN I view future weather conditions for that city
-THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, and the humidity
-WHEN I click on a city in the search history
-THEN I am again presented with current and future conditions for that city
-*/
-
-/**
- * When the page loads i can start my search using the text box or recently searched cities. if no cities have been searched present default values.
- * * NEED EVENT LISTENER to call first function
- * * ADD SEARCH TO RECENT SEARCH HISTORY
- *
- * once the search is executed on the web app, the appropriate data is passed through the Openweather API and results are returned
- * * API GET CALL
- * * API Results stored to local
- * * * City Name
- * * * Date
- * * * Icon of Weather Conditions (IN API???)
- * * * Temperature
- * * * Humidity
- * * * Wind Speed
- * * * UV Index
- * * * * * ICON for weather conditions (IN API???)
- *
- * Once results are returned i want to ensure the approprate icon is present for weather conditions and UV index.
- * * FUNCTION FOR CHECKING ICON OF WEATHER CONDITION
- * * FUNCTION FOR CHECKING ICON OF UV INDEX
- * *
- * * DIPLAYING RESULTS
- * * * first card:
- * * * * * Detailed View
- * * * Second card:
- * * * * * condensed view (Date, icon, temp, humidity)
- */
 var citiesSearched = [];
 var requestOptions = {
     method: 'GET',
@@ -50,10 +9,8 @@ var buttonSubmit = document.getElementById("search-button");
 //set common class for all buttons and then look for the event on that specific class
 buttonSubmit.addEventListener("click", () => getAPI())
 
-
 function setSearchedCity(city, results, data) {
     var temperature = results[0].main.temp;
-    //var date = results[0].dt_txt;
     var humidity = results[0].main.humidity;
     var windSpeed = results[0].wind.speed;
     var imageURL = results[0].weather[0].icon;
@@ -98,16 +55,12 @@ function setFiveDay(data) {
 };
 
 function getAPI() {
-
     var city = document.getElementById("search-input").value;
-    console.log(city);
     citiesSearched.unshift(city);
     citiesSearched.length = 4;
-    console.log(citiesSearched);
     localStorage.setItem("search", citiesSearched);
 
     var requestURL = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=acaa48cd5a14b6aa31f93113ccf288cc&units=imperial`;
-
 
     fetch(requestURL, requestOptions)
         .then(function (response) {
@@ -117,7 +70,6 @@ function getAPI() {
             var lat = data.city.coord.lat;
             var lon = data.city.coord.lon;
             var results = data.list;
-
 
             fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&exclude=hourly,minutely,alerts&appid=6b8690633d63e8bf4fb81fc2fa7b972d`, requestOptions)
                 .then(function (response) {
@@ -134,10 +86,9 @@ function getAPI() {
 
 function fixedSearch(city) {
 
-    console.log(city);
     citiesSearched.unshift(city);
     citiesSearched.length = 4;
-    console.log(citiesSearched);
+
     localStorage.setItem("search", citiesSearched);
 
     var requestURL = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=acaa48cd5a14b6aa31f93113ccf288cc&units=imperial`;
